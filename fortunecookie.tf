96f0ba1b771d9635e8bd263aac316a839faee8ed
alangley345 <alangley345@gmail.com>
2020-01-02 15:12:31 -0500

    fixed ip address and added key pair

--- fortunecookie.tf
+++ fortunecookie.tf
@@ -3,11 +3,17 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+
+#creates base for building out web app
 resource "aws_instance" "fortunecookie_base" {
   ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
-  associate_public_ip_address = true
+  associate_public_ip_address = "true"
+  key_name = "fortuneCookie"
+  
 }
+
+
 
 terraform {
 backend "s3" {
