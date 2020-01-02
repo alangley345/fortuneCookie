a0a37b7792731384459115fd07be568125ca68f5
alangley345 <alangley345@gmail.com>
2020-01-02 14:12:04 -0500

    modified:   fortunecookie.tf

--- fortunecookie.tf
+++ fortunecookie.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws instance" "fortunecookie_base" {
+resource "aws_instance" "fortunecookie_base" {
   ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
 }
