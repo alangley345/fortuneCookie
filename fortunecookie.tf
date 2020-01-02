183ef83e39015747a26e8999a152ca8450f74a90
alangley345 <alangley345@gmail.com>
2020-01-02 14:04:29 -0500

    renamed example to reflect project

---
+++ fortunecookie.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "fortunecookie" "baseconfig" {
+  ami           = "ami-00068cd7555f543d5"
+  instance_type = "t2.micro"
+}
+
+terraform {
+backend "s3" {
+bucket="myterraformcode"
+key="fortunecookie/terraform.tfstate"
+region="us-east-1"
+}
+}
