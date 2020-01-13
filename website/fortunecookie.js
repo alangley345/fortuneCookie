8a52962435902e6192fe305139247f92b491d603
Aaron Langley <aaron@x1Carbon.greatcatlab.net>
2020-01-12 21:42:11 -0500

    Reconfigured static site to include css and js, has refresh functionality.

---
+++ website/fortunecookie.js
@@ -0,0 +1,7 @@
+//hardcoded list of fortunes for now.
+var fortunes = ["You will accomplish much today", "Take risks because you have luck on your side", "The best relationships are formed by listening and learning"];
+
+//randomly selects array position from above string
+let randomValue = fortunes[Math.floor(Math.random() * fortunes.length)]
+
+document.getElementById("fortune").innerHTML = randomValue;
