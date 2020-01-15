b6b007ce584d07a1de1d8b6fc5ecd22cbc578459
Aaron Langley <aaron@x1Carbon.greatcatlab.net>
2020-01-14 21:37:20 -0500

    tweaking nginx.conf

--- website/fortunecookie.js
+++ website/fortunecookie.js
@@ -1,5 +1,6 @@
 //hardcoded list of fortunes for now.
-var fortunes = ["You will accomplish much today", "Take risks because you have luck on your side", "The best relationships are formed by listening and learning","Do you best for you", "Reach for the stars",];
+var fortunes = ["You will accomplish much today", "Take risks because you have luck on your side",
+ "The best relationships are formed by listening and learning","Do you best for you", "Reach for the stars",];
 
 //randomly selects array position from above string
 let randomValue = fortunes[Math.floor(Math.random() * fortunes.length)]
