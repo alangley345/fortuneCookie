f474c82e0346af66b508c32d9196b5d371b0c20e
Aaron Langley <alangley@gmail.com>
2022-07-06 14:50:53 -0400

    updated index.html

--- site/app.js
+++ site/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.listen(3000);
+app.use(express.static(__dirname + '/static'))
 
 app.get('/',function(req,res) {
-  res.sendFile(__dirname,'/index.html');
+  res.send('index.html');
 });
