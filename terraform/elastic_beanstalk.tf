df828ab6e87940dcebe1b9e890440252c0f1271f
Aaron Langley <alangley345@gmail.com>
2022-07-09 11:09:44 -0400

    Added dedicated sg for instance

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -7,10 +7,11 @@ resource "aws_elastic_beanstalk_application" "fortune-cookie" {
 # Create elastic beanstalk Environment
 
 resource "aws_elastic_beanstalk_environment" "fortune-cookie-env" {
-  name                = "fortune-cookie-env"
-  application         = aws_elastic_beanstalk_application.fortune-cookie.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.5.4 running Node.js 16"
-  tier                = "WebServer"
+  name                   = "fortune-cookie-env"
+  application            = aws_elastic_beanstalk_application.fortune-cookie.name
+  solution_stack_name    = "64bit Amazon Linux 2 v5.5.4 running Node.js 16"
+  tier                   = "WebServer"
+  wait_for_ready_timeout = "10m"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -22,6 +23,18 @@ resource "aws_elastic_beanstalk_environment" "fortune-cookie-env" {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.fortune-cookie-s3.id
+  }
+
+  setting {
+    namespace = "aws:elb:listener"
+    name      = "InstancePort"
+    value     = "3000"
+  }
+
+  setting {
+    namespace = "aws:elb:listener"
+    name      = "InstancePort"
+    value     = "3000"
   }
 
   setting {
@@ -46,26 +59,37 @@ resource "aws_elastic_beanstalk_environment" "fortune-cookie-env" {
     name      = "EnvironmentType"
     value     = "SingleInstance"
   }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t2.nano"
   }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SecurityGroups"
+    value     = aws_security_group.allow_nodejs.id
+  }
+
   setting {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
     value     = data.terraform_remote_state.base_state.outputs.prod_ext_vpc_id
   }
+
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
     value     = 1
   }
+
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = 1
   }
+  
   #setting {
   #  namespace = "aws:elasticbeanstalk:healthreporting:system"
   #  name      = "SystemType"
---
+++ terraform/sg_fortunecookie.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_nodejs" {
+  name        = "allow_nodejs"
+  description = "Allow traffic to Node.js for testing"
+  vpc_id      = data.terraform_remote_state.base_state.outputs.prod_ext_vpc_id
+
+  ingress {
+    description      = "Node.JS default port"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
