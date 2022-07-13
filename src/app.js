4657a5549754150608de7f110b4c35d1713fa0ab
Aaron Langley <alangley@gmail.com>
2022-07-13 12:49:57 -0400

    Removed node modules

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const bootstrap = require("./src/bootstrap");
+const express   = require("express");
+const app       = express();
+const router    = express.Router()
+const PORT      = 3000;
+const bootstrap = require("./src/bootstrap.js");
 
-bootstrap(app, router);
-
+app.use(router)
+bootstrap(app, router );
 app.listen(PORT,console.log("Server started on port: " + PORT));
+
+router.get("/",(req, res, next) => {
+  return res.send("Welcome to Fortune Cookie!")
+})
