96a0d369ba17b6ea487edbae0bd4e7272a17bd09
Aaron Langley <alangley345@gmail.com>
2022-07-17 22:50:48 -0400

    Added connection to mongodb ab

--- src/models/fortunesModel.js
+++ src/models/fortunesModel.js
@@ -1,6 +1,8 @@
 //hardcoded list of fortunes for now.
 //TO DO - move to dedicated DB later
 
+const { default: mongoose } = require("mongoose");
+
 const fortunes = [
     {"id":1, content:"You will accomplish much today."},
     {"id":2, content:"Take risks because you have luck on your side."},
@@ -10,11 +12,22 @@ const fortunes = [
     {"id":6, content:"Everyday begins with a new sunrise."}
 ];
 
-exports.getAllFortunes = () => {
-    return fortunes;
-};
+var fortunesSchema = new mongoose.Schema({
+    content: String,
+    contributor: String
+})
+
+var fortunesModel = mongoose.model('fortunes',fortunesSchema)
 
 exports.getNewFortune = () => {
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    return randomFortune
+    const fortune = fortunesModel.findOne();
 };
+
+//exports.getAllFortunes = () => {
+//    return fortunes;
+//};
+
+//exports.getNewFortune = () => {
+//  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+//  return randomFortune
+//};
